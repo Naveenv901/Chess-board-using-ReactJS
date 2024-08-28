3aa7a3c3a96ffd4fafc06e894a5cd90072bc624c
Naveenv901 <naveenv7010@gmail.com>
2024-08-28 11:21:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import "./App.css"
+import { FaChessKing } from "react-icons/fa";
+import { FaChessPawn } from "react-icons/fa";
+import { FaChessRook } from "react-icons/fa6";
+import { FaChessKnight } from "react-icons/fa";
+import { FaChessBishop } from "react-icons/fa";
+import { FaChessQueen } from "react-icons/fa";
+import { FaRegChessPawn } from "react-icons/fa6";
+import { FaRegChessRook } from "react-icons/fa6";
+import { FaRegChessKnight } from "react-icons/fa6";
+import { FaRegChessKing } from "react-icons/fa6";
+import { FaRegChessQueen } from "react-icons/fa6";
+import { FaRegChessBishop } from "react-icons/fa6";
+const arr=[<FaChessRook/>,<FaChessKnight />,<FaChessBishop />,<FaChessQueen />,<FaChessKing />,<FaChessBishop />,<FaChessRook/>]
+function Event(){
+  
 }
+function App(){
+  return(
+    <div className="Main">
+      <div>
+        <table>
+          <tr className="row1">
+            <td><FaChessRook/></td>
+            <td><FaChessKnight /></td>
+            <td><FaChessBishop /></td>
+            <td><FaChessQueen /></td>
+            <td><FaChessKing /></td>
+            <td><FaChessBishop /></td>
+            <td><FaChessKnight /></td>
+            <td><FaChessRook/></td>
+          </tr>
+          <tr className="row2">
+            <td> <FaChessPawn /></td>
+            <td> <FaChessPawn /></td>
+            <td> <FaChessPawn/></td>
+            <td> <FaChessPawn /></td>
+            <td> <FaChessPawn /></td>
+            <td> <FaChessPawn /></td>
+            <td> <FaChessPawn /></td>
+            <td> <FaChessPawn/></td>
+          </tr>
+          <tr className="row1">
+            <td onClick={()=>Event()}></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr className="row2">
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr className="row1">
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr className="row2">
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr className="row1">
+            <td><FaRegChessPawn /></td>
+            <td><FaRegChessPawn /></td>
+            <td><FaRegChessPawn /></td>
+            <td><FaRegChessPawn /></td>
+            <td><FaRegChessPawn /></td>
+            <td><FaRegChessPawn /></td>
+            <td><FaRegChessPawn /></td>
+            <td><FaRegChessPawn /></td>
+          </tr>
+          <tr className="row2">
+            <td><FaRegChessRook /></td>
+            <td>< FaRegChessKnight /></td>
+            <td><FaRegChessBishop /></td>
+            <td><FaRegChessKing /></td>
+            <td><FaRegChessQueen /></td>
+            <td><FaRegChessBishop /></td>
+            <td><FaRegChessKnight /></td>
+            <td><FaRegChessRook /></td>
+          </tr>
 
+        </table>
+      </div>
+    </div>
+  )
+}
 export default App;
